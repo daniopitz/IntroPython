aea4944b3ffd52d296dcfdf2135183c7a916a368
Daniela Opitz <calipsotornasol@frutilla.local>
2023-11-09 15:37:36 -0300

    clase 12

--- clases/clase12/03_readfile.py
+++ clases/clase12/03_readfile.py
@@ -8,13 +8,23 @@ Created on Wed May  4 11:28:44 2022
 comunas=[]
 contagiados=[]
 with open('2020-05-01-CasosConfirmados.csv', 'r') as file:
-    next(file)
+    next(file) #me salto la fila 1
     for line in file:
         if line.startswith('Metropolitana')==True:
-            print(line)
             lista_aux=line.split(',')
             comunas.append(lista_aux[2])
             contagiados.append(float(lista_aux[5]))
         
-print(contagiados)
-print(comunas)
+contagiados_back=contagiados.copy() #comentario
+contagiados.sort(reverse=True)
+contagiados_top5=contagiados[0:5]
+minimo=min(contagiados_top5)
+
+
+for k in contagiados_back:
+    if k >=minimo:
+        comuna=comunas[contagiados_back.index(k)]
+        print(comuna,k)
+        
+        
+
